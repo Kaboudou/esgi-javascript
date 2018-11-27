8725bfb6ebd4a0b070a4b6e16659f6f40773d235
Nakib K <kingamet91@gmail.com>
2018-11-27 10:34:10 +0100

    New commit

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -1,19 +1,31 @@
 
 
 function ucfirst(str) {
+    if( typeof str == "string") {
     var hello = str;
-    console.log(hello.toUpperCase().charAt(0) + hello.substr(1, hello.length));
+    return hello.toUpperCase().charAt(0) + hello.substr(1, hello.length);
+    else{
+            hello = "";
+            return hello;
+        }
+    //console.log(hello.toUpperCase().charAt(0) + hello.substr(1, hello.length));
 }
 function capitalize(str) {
+    if( typeof str == "string") {
     var hello = str;
     var tab = hello.split(" ");
-
     //var first = hello.toUpperCase().charAt(0) + hello.substr(1,space);
     for(var i in tab){
         tab[i]=tab[i].substr(0,1).toUpperCase()+tab[i].substr(1, tab[i].length);
     }
     hello = tab.join(" ");
     console.log(hello);
+    return hello;
+    }
+    else{
+        hello = "";
+        return hello;
+    }
     // while(){
     //     var mot = hello.toUpperCase().charAt(space+1) + hello.substr(space+2, hello.length);
     //     var space = hello.substr(space,hello.length).search(" ");
@@ -23,15 +35,20 @@ function capitalize(str) {
 
     }
     function camelCase(str){
-        var hello = str;
-        var tab = hello.split(" ");
-        var space = hello.search(" ");
-        for(var i in tab){
-            tab[i]=tab[i].substr(0,1).toUpperCase()+tab[i].substr(1, tab[i].length);
+        if( typeof str == "string") {
+            var hello = str;
+            var tab = hello.split(" ");
+            var space = hello.search(" ");
+            for (var i in tab) {
+                tab[i] = tab[i].substr(0, 1).toUpperCase() + tab[i].substr(1, tab[i].length);
+            }
+            hello = tab.join("");
+            return hello;
         }
-        hello = tab.join("");
-        console.log(hello);
-
+        else{
+            hello = "";
+            return hello;
+        }
     }
     function prop_access(){
 
